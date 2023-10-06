2c7384661fadaf62f4e9bcdb041593d1dca31218
Rohit kumar <126380249+Rohit20140048861@users.noreply.github.com>
2023-10-06 23:41:45 +0530

    Update InfoFrame.java

--- InfoFrame.java
+++ InfoFrame.java
@@ -1,70 +1,30 @@
 import java.awt.*;  // java.awt package is used     
 
 private class InfoFrame extends Frame  //user defined class(InfoFrame) and than extends Frame class (inheritance)
-// class InfoFrame 
+// class InfoFrame //without extends keyword
 {
 	InfoFrame()
 	{
-		this.setVisible(true);    // here we have provided all frame properties using this keyword 
-		this.setSize(400, 400);
-		this.setTitle("Student database");
-		this.setLayout(null);
+		// this.setVisible(true);    // here we have provided all frame properties using this keyword 
+		// this.setSize(400, 400);
+		// this.setTitle("Student database");
+		// this.setLayout(null);
         
-		// Color c1=new Color(255,0,0);        //using rgb value 
-		Color c2= Color(0Xceff00);  //using hexagonal value where 0X is neccessary before staring 
-		this.setBackground(c1);    //set the background color of frame
+		// // Color c1=new Color(255,0,0);        //using rgb value 
+		// Color c2= Color(0Xceff00);  //using hexagonal value where 0X is neccessary before staring 
+		// this.setBackground(c1);    //set the background color of frame
 		
-		
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		// Frame f = new Frame();  //give the frame 
 		// f.setVisible(true);  //set the visiblity
 		// f.setSize(400,400);
-		// f.setTitle(" Student Data base");
+		// f.setTitle(" Student Data base");   //title of the frame
 		// f.setLayout(null);
 		
-		// Label l =new Label("Name");
-		// l.setBounds(50,50,100,20);
+		// Label l =new Label("Name");  //provide label
+		// l.setBounds(50,50,100,20);   //provide the are (int x , int y, int width, int height)
 		// f.add(l);
 
-		// TextField t= new TextField();
+		// TextField t= new TextField();  //provide the area where you fill the answer
 		// t.setBounds(150,50,100,20);
 		// f.add(t);
 		
@@ -80,8 +40,8 @@ private class InfoFrame extends Frame  //user defined class(InfoFrame) and than 
 		// l3.setBounds(50,120,100,50);
 		// f.add(l3);
 		
-		// CheckboxGroup grp =new CheckboxGroup();
-		// Checkbox c1  =new Checkbox("Male",grp,false);
+		// CheckboxGroup grp =new CheckboxGroup();    //use for check or unchecked while answering radio button
+		// Checkbox c1  =new Checkbox("Male",grp,false);   // here false will fill empty and true will be fill already
 		// c1.setBounds(150,120,50,50);
 		// f.add(c1);
 		
@@ -93,15 +53,15 @@ private class InfoFrame extends Frame  //user defined class(InfoFrame) and than 
 		// l4.setBounds(50,180,100,50);
 		// f.add(l4);
 
-		// Choice t6  =new Choice();
+		// Choice t6  =new Choice();    // use for making choise 
 		// t6.setBounds(150,195,100,50);
-		// t6.add("10th");
+		// t6.add("10th");     // selecting multiple value in given choice
 		// t6.add("12th");
 		// t6.add("Graduate");
 		// f.add(t6);
 		
-		// Button b1 = new Button("Submit");
-		// b1.setBounds(80,250,100,40);
+		// Button b1 = new Button("Submit");    // creating button using it
+		// b1.setBounds(80,250,100,40);  
 		// f.add(b1);
 
 		// Button b2 = new Button("Cancel");
